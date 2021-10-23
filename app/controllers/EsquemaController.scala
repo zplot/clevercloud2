e7aaddab720177cde25af89cee49f5a19b5371bf
zplot <luisfontes@zplot.com>
2021-10-23 22:30:13 +0200

    Tenemos un modelo para usar
    Funciona bien

---
+++ app/controllers/EsquemaController.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import models._
+
+@Singleton
+class EsquemaController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+  def esquema = Action {
+
+    val datosParaPasar: Datos = Datos("Hola", 457, List(1,2,3,4,5,6,7))
+
+    val texto1 = "Título de la página"
+    val texto2 = "Esto está en la variable texto2 definida en EsquemaController"
+
+    val parameters: (String, String, Datos) = (texto1, texto2, datosParaPasar)
+
+    Ok(views.html.esquema(parameters))
+  }
+
+}
---
+++ app/models/Datos.scala
@@ -0,0 +1,7 @@
+package models
+
+case class Datos(parametro1: String, parametro2: Int, parametro3: List[Int]) {
+  val dato1 = parametro1
+  val dato2 = parametro2
+  val dato3 = parametro3
+}
