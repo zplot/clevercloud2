e8a9b3383c88d1928f17d045b4af5ea4f445b534
zplot <luisfontes@zplot.com>
2021-10-26 00:14:36 +0200

    vamos

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,19 +4,9 @@ import javax.inject._
 import play.api.mvc._
 import models._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index = Action {
 
     val datosParaPasar: Datos = Datos("Hola", 457, List(1,2,3,4,5,6,7))
@@ -29,5 +19,10 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.esquema(parameters))
   }
 
-
 }
+
+/*
+routes --> HomeController.index -->  esquema.scala.html -->  main5.scala.html
+esqueleto.scala.html es código html que se le pasa a mainEsquema, no es un eslabón de la cadena
+*/
+
