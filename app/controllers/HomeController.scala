80cea6feebd54222b2c1b87c64b73902161da084
zplot <luisfontes@zplot.com>
2021-11-12 17:14:34 +0100

    vamos

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,25 +3,33 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import models._
+import play.twirl.api.Html
+
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
 
-    val datosParaPasar: Datos = Datos("Hola", 457, List(1,2,3,4,5,6,7))
+  def pag1 = Action {
 
-    val pageTitle = "Math & Code"
-    val texto2 = "Esto está en la variable texto2 definida en EsquemaController"
+    val title = "Título de la página"
+    val header = "Cabecera de la página"
 
-    val parameters: (String, String, Datos) = (pageTitle, texto2, datosParaPasar)
 
-    Ok(views.html.generic.esquema(parameters))
+    val estoEsHtml: play.twirl.api.Html = views.html.generic.ejemplo()
+    val sinxfun: play.twirl.api.Html = views.html.specific.sinx()
+
+    val section1 = Section(1, "Cabecera de la sección 1", estoEsHtml)
+
+    val section2 = Section(2, "Cabecera de la sección 2", sinxfun)
+
+    val section3 = Section(3, "Cabecera de la sección 3", estoEsHtml)
+
+    val sections = List(section1, section2, section3)
+    val page1 = Page(title, header, sections)
+
+    Ok(views.html.generic.pag1(page1))
   }
 
 }
-
-/*
-routes --> HomeController.index -->  esquema.scala.html -->  main.scala.html
-*/
 
