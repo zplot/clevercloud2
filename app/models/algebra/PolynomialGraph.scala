354732eab5f06bb244d00140a5cb360ebc670630
zplot <luisfontes@zplot.com>
2021-11-20 10:54:23 +0100

    Ahora los nodes y edges son Sets

--- app/models/algebra/PolynomialGraph.scala
+++ app/models/algebra/PolynomialGraph.scala
@@ -6,24 +6,26 @@ case class PolyGraph(prime: Int, polynomial: GFdeX.T2) {
 
   val p: Int = prime
 
-  case class NodeFp(x: GFdeX.field.T2) extends Node[GFdeX.field.T2]
-  case class EdgeFp(n1: NodeFp, n2: NodeFp) extends Edge[GFdeX.field.T2]
+  case class NodeFp(x: GFdeX.field.T2) extends Node[GFdeX.field.T2] {
+    override def toString: String = x.toString
+  }
+  case class EdgeFp(n1: NodeFp, n2: NodeFp) extends Edge[GFdeX.field.T2] {
+    override def toString: String = n1.toString + " --> " + n2.toString
+  }
 
   def evalPoly(x: GFdeX.field.T2): GFdeX.field.T2 = polynomial.evaluate(x)
 
-  val nodes: List[NodeFp] = (0 until p).toList.map(x => NodeFp(GFdeX.field.builder(x)))
-  val edges: List[EdgeFp] = (0 until p).toList.map(x => EdgeFp(NodeFp(GFdeX.field.builder(x)), NodeFp(evalPoly(GFdeX.field.builder(x)))))
+  val nodes: Set[NodeFp] = (0 until p).toList.map(x => NodeFp(GFdeX.field.builder(x))).toSet
+  val edges: Set[EdgeFp] = (0 until p).toList.map(x => EdgeFp(NodeFp(GFdeX.field.builder(x)), NodeFp(evalPoly(GFdeX.field.builder(x))))).toSet
 
 }
 
 object PolynomialGraph extends App {
 
-  val p = 5
+  val p = 11
   val cuerpo: Fp = Fp(p)
 
   val GFdeX: PolynomialsOverFp = PolynomialsOverFp(cuerpo)
-
-  val prueba: GFdeX.field.T2 = GFdeX.field.builder(7)
 
   def util(x: Int): GFdeX.field.T2 = GFdeX.field.builder(x)
 
