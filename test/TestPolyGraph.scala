ab910c41567468c57a70a8ac0b48af38840ca697
zplot <luisfontes@zplot.com>
2021-11-20 11:22:17 +0100

    vamos

--- test/TestPolyGraph.scala
+++ test/TestPolyGraph.scala
@@ -1,2 +1,11 @@
-object TestPolyGraph {
+import models.algebra._
+
+object TestPolyGraph extends App {
+
+  val f = Map(2 -> 1)
+  println(PolynomialGraph(5, f))
+  println(PolynomialGraph(5, f).nodes)
+  println(PolynomialGraph(5, f).edges)
+
 }
+
